9e0d72fdf4dfd10efc713b2c3281fadb4f4e352f
DmytroKorolyov <148568611+DmytroKorolyov@users.noreply.github.com>
2024-01-23 18:53:18 +0200

    1

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', submitFoo);
+
+function submitFoo(event) {
+  event.preventDefault();
+  const delay = form.elements.delay.value;
+  const state = document.querySelector('input[name="state"]:checked').value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.show({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#59A10D',
+        messageColor: '#FFFFFF',
+      });
+    })
+    .catch(delay => {
+      iziToast.show({
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+        backgroundColor: '#EF4040',
+        messageColor: '#FFFFFF',
+      });
+    });
+}
